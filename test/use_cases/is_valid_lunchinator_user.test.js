0c32e4d535c01d6a607723f7959770faf0a575f4
Richard Race <richard.race@madetech.com>
2019-07-23 17:55:16 +0100

    Add some more tests to ensure correct logic

--- test/use_cases/is_valid_lunchinator_user.test.js
+++ test/use_cases/is_valid_lunchinator_user.test.js
@@ -1,12 +1,24 @@
-const { expect } = require("../test_helper");
+const { expect, sinon, config } = require("../test_helper");
 const IsValidLunchinatorUser = require("@use_cases/is_valid_lunchinator_user");
 
 describe("IsValidLunchinatorUser", function() {
   it("check lunchinator user is valid", function() {
-    var useCase = new IsValidLunchinatorUser({ user_id: "U2147483697" });
+    const useCase = new IsValidLunchinatorUser({ user_id: config.VALID_SLACK_USER_IDS[0] });
 
-    var isValid = useCase.execute();
+    expect(useCase.execute()).to.be.true;
+  });
 
-    expect(isValid).to.be.true;
+  it("returns false if there are no valid Slack users", function() {
+    const userId = Object.assign({}, config).VALID_SLACK_USER_IDS[0];
+    const useCase = new IsValidLunchinatorUser({ user_id: userId });
+
+    sinon.stub(config, "VALID_SLACK_USER_IDS").get(() => []);
+    expect(useCase.execute()).to.be.false;
+  });
+
+  it("returns false if it is not a valid Slack user", function() {
+    const useCase = new IsValidLunchinatorUser({ user_id: "BogusID" });
+
+    expect(useCase.execute()).to.be.false;
   });
 });
