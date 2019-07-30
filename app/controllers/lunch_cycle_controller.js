57253392b5bf69b5d5be08551bda41162c0be47d
Matt Millar <matthew.millar@madetech.com>
2019-07-30 13:15:30 +0100

    Moved implementation out of lunch_cycle_controller

--- app/controllers/lunch_cycle_controller.js
+++ app/controllers/lunch_cycle_controller.js
@@ -1,37 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
-const { InMemoryLunchCycleGateway, GoogleSheetGateway } = require("@gateways");
-const {
-  GetNewLunchCycleRestaurants,
-  GetPreviousLunchCycle,
-  FetchRestaurantsFromGoogleSheet
-} = require("@use_cases");
+const { LunchCycleService } = require("@services");
 
 router.post("/new", async function(req, res) {
   console.log(req.headers);
-  console.log(req.statusCode);
   console.log(req.body);
 
-  const getNewLunchCycleRestaurants = new GetNewLunchCycleRestaurants({
-    fetchRestaurantsFromGoogleSheet: new FetchRestaurantsFromGoogleSheet({
-      googleSheetGateway: new GoogleSheetGateway()
-    }),
-    getPreviousLunchCycle: new GetPreviousLunchCycle({
-      lunchCycleGateway: new InMemoryLunchCycleGateway()
-    })
-  });
-
-  const lastRestaurantInLastLunchCycle = null;
-  const getNewLunchCycleRestaurantsResponse = await getNewLunchCycleRestaurants.execute(
-    lastRestaurantInLastLunchCycle
-  );
-
-  const message = getNewLunchCycleRestaurantsResponse.restaurants
-    .map(restaurants => {
-      return `${restaurants.emoji} ${restaurants.name}`;
-    })
-    .join("\n");
+  const lunchCycleService = new LunchCycleService();
+  if (!lunchCycleService.verifySlackRequest(req.headers, req.body)) {
+    res.send("ERROR");
+  }
+  const lunchCycleRestaurants = await lunchCycleService.getLunchCycleRestaurants();
+  const message = lunchCycleService.getPreviewMessage(lunchCycleRestaurants);
 
   res.send(`Next Cycle Restaurants:\n${message}`);
 });
