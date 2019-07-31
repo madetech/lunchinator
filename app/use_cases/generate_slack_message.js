831edf9fe832d3d62e161ff31566501afe11d1f4
George Schena <40758489+georgeschena@users.noreply.github.com>
2019-07-31 15:53:44 +0100

    Remove generate_slack_preview_message uscase  (#7)

--- app/use_cases/generate_slack_message.js
+++ app/use_cases/generate_slack_message.js
@@ -1,18 +1,22 @@
+const moment = require("moment");
+
 class GenerateSlackMessage {
   constructor(options) {
     this.firstName = options.firstName;
   }
 
   execute({ lunchCycle }) {
-    const twentFourHoursInMs = 86400000;
-
-    let message = `Hey ${this.firstName} it’s time to enter the draw for the next cycle of company lunches. Let us know which dates you’ll be available on by reacting with the matching emoji.\n\n`;
+    let firstName = this.firstName;
+    if (firstName === null) {
+      firstName = "{first name}";
+    }
+    let message = `Hey ${firstName}! It’s time to enter the draw for the next cycle of company lunches. Let us know which dates you’ll be available on by reacting with the matching emoji.\n\n`;
 
     lunchCycle.restaurants.forEach((r, i) => {
-      const nextDate = new Date(lunchCycle.starts_at.getTime() + i * 7 * twentFourHoursInMs);
-      const dateString = `${nextDate.getDate()}/${nextDate.getMonth() +
-        1}/${nextDate.getFullYear()}`;
-      message += `${r.emoji} ${dateString} ${r.name} vegan:${r.dietaries.vegan}, meat:${r.dietaries.meat}, direction:${r.direction}\n`;
+      const nextDate = moment.utc(lunchCycle.starts_at).add(i * 7, "days");
+      message += `${r.emoji} ${nextDate.format("DD/MM/YYYY")} ${r.name} vegan:${
+        r.dietaries.vegan
+      }, meat:${r.dietaries.meat}, direction:${r.direction}\n`;
     });
 
     return {
--- test/use_cases/generate_slack_message.test.js
+++ test/use_cases/generate_slack_message.test.js
@@ -13,29 +13,40 @@ const restaurantList = [
 ];
 
 describe("GenerateSlackMessage", function() {
-  it("can generate a lunch cycle message", function() {
-    const lunchCycle = new LunchCycle({
-      restaurants: restaurantList,
-      starts_at: new Date("2020-03-12T00:00:00")
-    });
+  const lunchCycle = new LunchCycle({
+    restaurants: restaurantList,
+    starts_at: new Date("2020-03-12T00:00:00")
+  });
 
+  const direction = "googlemaps";
+  const expected = [
+    `:bowtie: 12/03/2020 restaurant1 vegan:2, meat:2, direction:${direction}`,
+    `:smile: 19/03/2020 restaurant2 vegan:2, meat:2, direction:${direction}`,
+    `:simple_smile: 26/03/2020 restaurant3 vegan:2, meat:2, direction:${direction}`,
+    `:laughing: 02/04/2020 restaurant4 vegan:2, meat:2, direction:${direction}`,
+    `:blush: 09/04/2020 restaurant5 vegan:2, meat:2, direction:${direction}`,
+    `:relaxed: 16/04/2020 restaurant6 vegan:2, meat:2, direction:${direction}\n`
+  ];
+
+  it("can generate a lunch cycle message with a first name", function() {
     const slackFirstName = "Barry";
-
     const useCase = new GenerateSlackMessage({ firstName: slackFirstName });
     const response = useCase.execute({ lunchCycle: lunchCycle });
-    const direction = "googlemaps";
-
-    const expected = [
-      `:bowtie: 12/3/2020 restaurant1 vegan:2, meat:2, direction:${direction}`,
-      `:smile: 19/3/2020 restaurant2 vegan:2, meat:2, direction:${direction}`,
-      `:simple_smile: 26/3/2020 restaurant3 vegan:2, meat:2, direction:${direction}`,
-      `:laughing: 2/4/2020 restaurant4 vegan:2, meat:2, direction:${direction}`,
-      `:blush: 9/4/2020 restaurant5 vegan:2, meat:2, direction:${direction}`,
-      `:relaxed: 16/4/2020 restaurant6 vegan:2, meat:2, direction:${direction}\n`
-    ];
 
     const expectedMessage =
-      `Hey ${slackFirstName} it’s time to enter the draw for the next cycle of company lunches. Let us know which dates you’ll be available on by reacting with the matching emoji.\n\n` +
+      `Hey ${slackFirstName}! It’s time to enter the draw for the next cycle of company lunches. Let us know which dates you’ll be available on by reacting with the matching emoji.\n\n` +
+      expected.join("\n");
+
+    expect(response.message).to.be.eql(expectedMessage);
+  });
+
+  it("can generate a lunch cycle message without a first name", function() {
+    const noFirstName = null;
+    const useCase = new GenerateSlackMessage({ firstName: noFirstName });
+    const response = useCase.execute({ lunchCycle: lunchCycle });
+
+    const expectedMessage =
+      `Hey {first name}! It’s time to enter the draw for the next cycle of company lunches. Let us know which dates you’ll be available on by reacting with the matching emoji.\n\n` +
       expected.join("\n");
 
     expect(response.message).to.be.eql(expectedMessage);
