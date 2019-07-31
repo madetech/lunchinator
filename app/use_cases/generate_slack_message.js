c98680e517e14f90ef5fd811065b5d251b57533f
George Schena <40758489+georgeschena@users.noreply.github.com>
2019-07-31 12:07:50 +0100

    Generating a slack message (#5)

---
+++ app/use_cases/generate_slack_message.js
@@ -0,0 +1,21 @@
+class GenerateSlackMessage {
+  execute({ lunchCycle, slackUser }) {
+    const twentFourHoursInMs = 86400000;
+    const slackFirstName = slackUser.profile.first_name;
+
+    let message = `Hey ${slackFirstName}! It’s time to enter the draw for the next cycle of company lunches. Let us know which dates you’ll be available on by reacting with the matching emoji.\n\n`;
+
+    lunchCycle.restaurants.forEach((r, i) => {
+      const nextDate = new Date(lunchCycle.starts_at.getTime() + i * 7 * twentFourHoursInMs);
+      const dateString = `${nextDate.getDate()}/${nextDate.getMonth() +
+        1}/${nextDate.getFullYear()}`;
+      message += `${r.emoji} ${dateString} ${r.name} vegan:${r.dietaries.vegan}, meat:${r.dietaries.meat}, direction:${r.direction}\n`;
+    });
+
+    return {
+      message: message
+    };
+  }
+}
+
+module.exports = GenerateSlackMessage;
