f6a752276a2c7152af94fe26547896a9619abce9
Richard Race <richard.race@madetech.com>
2019-08-01 10:24:36 +0100

    Correct file path

--- app/gateways/index.js
+++ app/gateways/index.js
@@ -4,6 +4,6 @@ module.exports = {
   SlackGateway: require("./slack_gateway"),
   ...require("./google_sheet_gateway"),
   CryptoGateway: require("./crypto_gateway"),
-  InMemorySlackUserResponseGateway: require("./in_memory_slack_user_lunch_cycle_gateway"),
+  InMemorySlackUserResponseGateway: require("./in_memory_slack_user_response_gateway"),
   PostgresSlackUserResponseGateway: require("./postgres_slack_user_response_gateway")
 };
