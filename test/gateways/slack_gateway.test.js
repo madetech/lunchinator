d8a40a3cb72d42f177c0adfeba871b6be99027c4
cob16 <public+github@cormacbrady.info>
2019-12-10 14:32:07 +0000

    do not enter multi channel guests into draws

--- test/gateways/slack_gateway.test.js
+++ test/gateways/slack_gateway.test.js
@@ -187,6 +187,59 @@ describe("SlackGateway", function() {
     ]);
   });
 
+  it("can filter users that are 'restricted'", async function() {
+    userList = [
+      {
+        id: "USLACKID1",
+        team_id: "TEAM_ID",
+        name: "Test Name",
+        deleted: false,
+        profile: {
+          email: "test1@example.com"
+        },
+        is_bot: false,
+        is_app_user: false,
+        is_restricted: true,
+        is_ultra_restricted: false,
+        updated: 1520258399
+      },
+      {
+        id: "USLACKID2",
+        team_id: "TEAM_ID",
+        name: "Bob",
+        deleted: false,
+        profile: {
+          email: "test@example.com"
+        },
+        is_bot: false,
+        is_app_user: false,
+        is_restricted: false,
+        is_ultra_restricted: false,
+        updated: 1550160376
+      }
+    ];
+
+    fakeSlackClient.stubUserList(userList);
+    const gateway = new SlackGateway();
+
+    expect(await gateway.fetchUsers()).to.eql([
+      {
+        id: "USLACKID2",
+        team_id: "TEAM_ID",
+        name: "Bob",
+        deleted: false,
+        profile: {
+          email: "test@example.com"
+        },
+        is_bot: false,
+        is_app_user: false,
+        is_restricted: false,
+        is_ultra_restricted: false,
+        updated: 1550160376
+      }
+    ]);
+  });
+
   it("can filter users that are 'ultra restricted'", async function() {
     userList = [
       {
